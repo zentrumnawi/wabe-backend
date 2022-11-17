5e0e81f135f48075631e4e60452fa8806ab604f9
Christian Grossmüller <chgad.games@gmail.com>
2022-11-17 19:27:16 +0100

    Added Admin entries.

--- wabe_content/admin.py
+++ wabe_content/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
-from .models import Word
+from .models import Word, Tone, Meaning
 # Register your models here.
+
+
+class ToneAdminInline(admin.StackedInline):
+    model = Tone
+
+
+class MeaningAdminInline(admin.StackedInline):
+    model = Meaning
 
 
 class WordAdmin(admin.ModelAdmin):
     model = Word
+    inlines = [ToneAdminInline, MeaningAdminInline]
+
+
+class ToneAdmin(admin.ModelAdmin):
+    model = Tone
+
+
+class MeaningAdmin(admin.ModelAdmin):
+    model = Meaning
 
 
 admin.site.register(Word, WordAdmin)
+admin.site.register(Tone, ToneAdmin)
+admin.site.register(Meaning, MeaningAdmin)

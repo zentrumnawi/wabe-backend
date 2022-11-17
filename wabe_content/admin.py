c93cd8962718bcea81b96cfd9075623666d3f0c5
Christian Grossmüller <chgad.games@gmail.com>
2022-11-17 18:12:43 +0100

    Create Word Model

--- wabe_content/admin.py
+++ wabe_content/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Word
 # Register your models here.
+
+
+class WordAdmin(admin.ModelAdmin):
+    model = Word
+
+
+admin.site.register(Word, WordAdmin)
--- wabe_content/models.py
+++ wabe_content/models.py
@@ -1,1 +1,38 @@
 from django.db import models
+from solid_backend.content.models import SolidBaseProfile
+from django.utils.translation import ugettext_lazy as _
+
+
+class Word(SolidBaseProfile):
+
+    name = models.CharField(max_length=200, verbose_name=_("Titel"))
+
+    graphic = models.TextField(
+        max_length=500,
+        null=True,
+        blank=True,
+        verbose_name=_("Graphie"),
+    )
+    lexem = models.TextField(
+        max_length=500,
+        null=True,
+        blank=True,
+        verbose_name=_("Lexem"),
+    )
+    etymology = models.TextField(
+        max_length=500,
+        null=True,
+        blank=True,
+        verbose_name=_("Etymologie"),
+    )
+    semantics = models.TextField(
+        max_length=500,
+        null=True,
+        blank=True,
+        verbose_name=_("Semantik"),
+    )
+
+    class Meta:
+        verbose_name = _("Wort")
+        verbose_name_plural = _("Wörter")
+

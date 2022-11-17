22bf7858787fc4abf00176c6f4f409760f25149e
Christian Grossmüller <chgad.games@gmail.com>
2022-11-17 19:34:18 +0100

    Create Serializers for models.

--- wabe_content/serializers.py
+++ wabe_content/serializers.py
@@ -1,5 +1,37 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from solid_backend.media_object.serializers import MediaObjectSerializer
+
+from .models import Word, Tone, Meaning
 
 
-class WordSerializer(ModelSerializer):
-    pass
+class DisplayNameModelSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        ret = super(DisplayNameModelSerializer, self).to_representation(instance)
+
+        return serializers.OrderedDict(filter(lambda x: not x[1] is None, ret.items()))
+
+
+class ToneSerializer(DisplayNameModelSerializer):
+
+    class Meta:
+        model = Tone
+        exclude = ["word"]
+        swagger_schema_fields = {"title": str(model._meta.verbose_name)}
+
+
+class MeaningSerializer(DisplayNameModelSerializer):
+    class Meta:
+        model = Meaning
+        exclude = ["word"]
+        swagger_schema_fields = {"title": str(model._meta.verbose_name)}
+
+
+class WordSerializer(DisplayNameModelSerializer):
+    tone = ToneSerializer()
+    meaning = MeaningSerializer()
+    media_objects = MediaObjectSerializer(many=True)
+    
+    class Meta:
+        model = Word
+        fields = "__all__"
+        swagger_schema_fields = {"title": str(model._meta.verbose_name)}

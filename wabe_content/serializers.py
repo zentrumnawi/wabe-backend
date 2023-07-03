ca0495bc73405e561a4d61852be47af1e4fee3f9
Christian Grossmüller <chgad.games@gmail.com>
2023-07-03 17:53:01 +0200

    Remove no longer needed field classes.

--- wabe_content/serializers.py
+++ wabe_content/serializers.py
@@ -1,20 +1,7 @@
-from rest_framework import serializers
 from solid_backend.media_object.serializers import MediaObjectSerializer
 from solid_backend.utils.serializers import SolidModelSerializer
 
 from .models import Word, Tone, Meaning
-
-
-class MDStringField(serializers.CharField):
-
-    class Meta:
-        swagger_schema_fields = {
-            "type": "mdstring"
-        }
-
-
-class MDField(serializers.CharField):
-    pass
 
 
 class ToneSerializer(SolidModelSerializer):

be682304653d275d3744fecd4b9038df2df453bf
Christian Grossmüller <chgad.games@gmail.com>
2023-07-03 17:51:45 +0200

    Remove code that was moved to solid-backend and include SolidModelSerializer.

--- wabe_content/serializers.py
+++ wabe_content/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from solid_backend.media_object.serializers import MediaObjectSerializer
+from solid_backend.utils.serializers import SolidModelSerializer
 from drf_spectacular.extensions import OpenApiSerializerFieldExtension, OpenApiSerializerExtension
 
 from .models import Word, Tone, Meaning, MDTextField
@@ -17,14 +18,7 @@ class MDField(serializers.CharField):
     pass
 
 
-class DisplayNameModelSerializer(serializers.ModelSerializer):
-    def to_representation(self, instance):
-        ret = super(DisplayNameModelSerializer, self).to_representation(instance)
-
-        return serializers.OrderedDict(filter(lambda x: not x[1] in (None, ""), ret.items()))
-
-
-class ToneSerializer(DisplayNameModelSerializer):
+class ToneSerializer(SolidModelSerializer):
 
     serializer_field_mapping = {
         **serializers.ModelSerializer.serializer_field_mapping,
@@ -34,10 +28,9 @@ class ToneSerializer(DisplayNameModelSerializer):
     class Meta:
         model = Tone
         exclude = ["word"]
-        swagger_schema_fields = {"title": str(model._meta.verbose_name)}
 
 
-class MeaningSerializer(DisplayNameModelSerializer):
+class MeaningSerializer(SolidModelSerializer):
 
     serializer_field_mapping = {
         **serializers.ModelSerializer.serializer_field_mapping,
@@ -47,10 +40,9 @@ class MeaningSerializer(DisplayNameModelSerializer):
     class Meta:
         model = Meaning
         exclude = ["word"]
-        swagger_schema_fields = {"title": str(model._meta.verbose_name)}
 
 
-class WordSerializer(DisplayNameModelSerializer):
+class WordSerializer(SolidModelSerializer):
 
     tone = ToneSerializer()
     meaning = MeaningSerializer()
@@ -63,24 +55,3 @@ class WordSerializer(DisplayNameModelSerializer):
     class Meta:
         model = Word
         fields = "__all__"
-        swagger_schema_fields = {"title": str(model._meta.verbose_name)}
-
-
-class TitleSerializerExtension(OpenApiSerializerExtension):
-    target_class = "wabe_content.serializers.DisplayNameModelSerializer"
-    match_subclasses = True
-
-    def map_serializer(self, auto_schema, direction):
-        schema = auto_schema._map_serializer(self.target, direction, bypass_extensions=True)
-        if hasattr(self.target, "Meta"):
-            schema["title"] = self.target.Meta.model._meta.verbose_name
-        return schema
-
-
-class MdFieldExtension(OpenApiSerializerFieldExtension):
-    target_class = "wabe_content.serializers.MDField"
-
-    def map_serializer_field(self, auto_schema, direction):
-        schema = auto_schema._map_serializer_field(self.target, direction, bypass_extensions=True)
-        schema['format'] = 'mdstring'
-        return schema
